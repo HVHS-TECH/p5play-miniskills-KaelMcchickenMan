27d0481cbddab6a169b7cf059581ed7425bf4e41
KaelMcchickenMan <23218km@hvhs.school.nz>
2026-02-09 12:57:04 +1300

    tomato sauce

--- t02_move_sprite.js
+++ t02_move_sprite.js
@@ -18,6 +18,9 @@ Circle.color = '200, 155, 55';
 Rectangle.rotationSpeed = 2;
 
 Rectangle.vel.x = 0.25;
+Rectangle.vel.y = -2;
+Circle.vel.x = 2;
+Circle.vel.y = -8;
 }
 	
 /*******************************************************/
