8616ea15990fb217437b0af00c82561ae326b488
KaelMcchickenMan <23218km@hvhs.school.nz>
2026-02-09 13:12:47 +1300

    reh

--- t03_gravity.js
+++ t03_gravity.js
@@ -32,7 +32,7 @@ Floor3 = new Sprite(200, 75, 2,180, 's');
 
 Floor3.color = 'rgb(148, 108, 223)';
 Tomato.rotationSpeed = 2;	
-Tomato.bounciness = 1.25;	
+Tomato.bounciness = 0.96;	
 Rectangle.rotationSpeed = 2;
 
 Rectangle.vel.x = 0.25;
