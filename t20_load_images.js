dc8720ef31bab2b4ef0850a03aa0c3fc05e3d937
KaelMcchickenMan <23218km@hvhs.school.nz>
2026-02-12 10:59:14 +1300

    yes

--- t20_load_images.js
+++ t20_load_images.js
@@ -9,9 +9,9 @@
 /*******************************************************/
 function preload() {
 
-	imgFace = loadImage('assets/images/face.png');
+	imgFace = loadImage('../assets/images/face.png');
 
-  imgBG  = loadImage('assets/images/space.jpg');
+  imgBG  = loadImage('../assets/images/space.jpg');
 
   
 
@@ -20,7 +20,7 @@ function preload() {
 function setup() {
 	console.log("setup: ");
 cnv = new Canvas(700, 700);
-ball_1 = new Sprite(width/2, height/2, 50, 'd');
+ball_1 = new Sprite(50, 50, 50, 'd');
 
 ball_1.bounciness = 1;
 
--- t21_head2Mouse.js
+++ t21_head2Mouse.js
@@ -9,14 +9,38 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+cnv = new Canvas(700, 700);
 
+world.gravity.y = 3;
+	Rectangle = new Sprite(99, 15, 25, 25);
+Rectangle.color = 'rgb(0, 255, 0)';
+Circle = new Sprite(125, 25, 25);
+
+Circle.color = 'rgb(131, 79, 255)';
+Tomato = new Sprite(150, 50, 25,75, 'd');
+
+Tomato.color = 'rgb(243, 158, 0)';
+
+Tomato.rotationSpeed = 2;	
+Tomato.bounciness = 0.96;	
+Rectangle.rotationSpeed = 2;
+
+Rectangle.vel.x = 0.25;
+Rectangle.vel.y = 0.1;
+Circle.vel.x = 0.1;
+Circle.vel.y = 0.05;
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('rgb(235, 95, 95)'); 
+if (mouse.pressing()) {
+
+Circle.moveTo(mouseX, mouseY, 5);
+
+}
 }
 
 /*******************************************************/
