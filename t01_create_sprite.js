7897b517885d9415dd2911defc9ac7197dcf9d59
KaelMcchickenMan <23218km@hvhs.school.nz>
2026-02-13 12:29:34 +1300

    yes

--- t01_create_sprite.js
+++ t01_create_sprite.js
@@ -9,12 +9,30 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
-cnv = new Canvas(windowWidth, windowHeight);
-	Rectangle = new Sprite(99, 15, 25, 25);
-Rectangle.color = '200, 155, 55';
-Circle = new Sprite(75, 75, 25);
+width = random(300,600);
+height = random(300,600);
 
-Circle.color = '200, 155, 55';
+// and the sprites keep changing too!
+spriteX= random(50,150);
+spriteY=  random(50,150);  ;
+
+cnv = new Canvas(width, height);
+
+
+red = new Sprite(spriteX/2, spriteY/2, spriteX, spriteY);
+red.color = 'red';
+
+green = new Sprite(width - spriteX/2, spriteY/2, spriteX, spriteY);
+green.color = 'green';
+
+blue = new Sprite(width -spriteX/2, height - spriteY/2, spriteX, spriteY);
+blue.color = 'blue';
+
+yellow = new Sprite(spriteX/2, height - spriteY/2,  spriteX, spriteY);
+yellow.color = 'yellow';
+
+purple = new Sprite(width/2 - spriteX/2, height/2 - spriteY/2,  spriteX, spriteY);
+purple.color = 'purple';
 }
 	
 /*******************************************************/
@@ -22,7 +40,7 @@ Circle.color = '200, 155, 55';
 /*******************************************************/
 function draw() {
 	
-background('ccc'); 
+background('rgb(235, 95, 95)');
 }
 
 /*******************************************************/
--- t22_keyboard.js
+++ t22_keyboard.js
@@ -9,15 +9,44 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+	cnv = new Canvas(700, 700);
 
+	world.gravity.y = 50;
+
+	Circle = new Sprite(125, 25, 25, 25, 'd');
+
+	Circle.color = 'rgb(131, 79, 255)';
+
+	Floor = new Sprite(350, 670, 700, 75, 'k')
+	Floor.color = 'rgb(255, 0, 0)';
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
+	background('rgb(235, 95, 95)'); 
+Circle.vel.x = Circle.vel.x / 1.1;
+Circle.vel.y = Circle.vel.y / 1.1;
+Circle.roatationSpeed = Circle.rotationSpeed / 1.05;
+	if (kb.pressing ('left')) {
+
+		Circle.vel.x = -3;
+		Circle.rotationSpeed = -5;
+
+	} 
+	if (kb.pressing ('right')) {
+
+	Circle.vel.x = 3;
+	Circle.rotationSpeed = 5;
+	} 
+	if (kb.pressing ('up') && Circle.y <= 600) {
+
+	Circle.vel.y = -25;
+	} 
 	
-}
+	}
+
 
 /*******************************************************/
 //  END OF APP
