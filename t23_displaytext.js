13e0533e722143448a45b02c88cab122da75c826
KaelMcchickenMan <23218km@hvhs.school.nz>
2026-02-17 14:24:24 +1300

    osh

---
+++ t23_displaytext.js
@@ -0,0 +1,54 @@
+/*******************************************************/
+// P5.play: t23_displaytext
+// Display text
+// Written by ???
+/*******************************************************/
+	
+/*******************************************************/
+// setup()
+/*******************************************************/
+function setup() {
+	console.log("setup: ");
+	cnv = new Canvas(700, 700);
+
+	world.gravity.y = 50;
+
+	Circle = new Sprite(125, 25, 25, 25, 'd');
+
+	Circle.color = 'rgb(131, 79, 255)';
+
+	Floor = new Sprite(350, 670, 700, 75, 'k')
+	Floor.color = 'rgb(255, 0, 0)';
+}
+	
+/*******************************************************/
+// draw()
+/*******************************************************/
+function draw() {
+	background('rgb(235, 95, 95)'); 
+Circle.vel.x = Circle.vel.x / 1.1;
+Circle.vel.y = Circle.vel.y / 1.1;
+Circle.roatationSpeed = Circle.rotationSpeed / 1.05;
+	if (kb.pressing ('left')) {
+
+		Circle.vel.x = -3;
+		Circle.rotationSpeed = -5;
+
+	} 
+	if (kb.pressing ('right')) {
+
+	Circle.vel.x = 3;
+	Circle.rotationSpeed = 5;
+	} 
+	if (kb.pressing ('up') && Circle.y >= 625) {
+
+	Circle.vel.y = -25;
+	} 
+	text("Hello World", 50, 50) 
+
+}
+
+
+/*******************************************************/
+//  END OF APP
+/*******************************************************/
