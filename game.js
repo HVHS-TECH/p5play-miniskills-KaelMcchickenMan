873a94aae3ce8698639b967fcf30eb265a736222
KaelMcchickenMan <23218km@hvhs.school.nz>
2026-02-19 10:59:54 +1300

    yesss

--- game.js
+++ game.js
@@ -1,13 +1,4 @@
-/*******************************************************/
-// P5.play: t23_displaytext
-// Display text
-// Written by ???
-/*******************************************************/
-	
-/*******************************************************/
-// setup()
-/*******************************************************/
-
+//BEGIN
 function setup() {
 	 let isOnFloor = 0;
 	console.log("setup: ");
@@ -16,13 +7,18 @@ function setup() {
 	world.gravity.y = 25;
 	floorGroup = new Group();
 	hitboxGroup = new Group();
+    ladderGroup = new Group();
 	
+    
 
 // BACKGROUND
 
 	Bg = new Sprite(400, 550, 50, 200, 'n')
 	Bg.color = 'rgb(156, 0, 0)';
 	Bg.strokeWeight = 0; 
+    Bg2 = new Sprite(190, 580, 50, 200, 'n')
+	Bg2.color = 'rgb(156, 0, 0)';
+	Bg2.strokeWeight = 0; 
 
 // FLOOR
 
@@ -37,6 +33,13 @@ function setup() {
 	Floor3 = new Sprite(400, 450, 50, 20, 'k')
 	Floor3.color = 'rgb(255, 0, 0)';
 	Floor3.strokeWeight = 0; 
+
+    Floor4 = new Sprite(190, 475, 50, 20, 'k')
+	Floor4.color = 'rgb(255, 0, 0)';
+	Floor4.strokeWeight = 0; 
+    Floor5 = new Sprite(155, 565, 20, 200, 'k')
+	Floor5.color = 'rgb(255, 0, 0)';
+	Floor5.strokeWeight = 0; 
 	
 // HITBOXES
 	hitbox = new Sprite(350, 636, 700, 10, 'k')
@@ -54,10 +57,18 @@ function setup() {
 	hitbox3.strokeWeight = 0; 
 	hitbox3.color.setAlpha(1);
 
-	
+    hitbox4 = new Sprite(180, 468, 70, 10, 'k')
+	hitbox4.color = 'rgb(255, 0, 242)';
+	hitbox4.strokeWeight = 0; 
+	hitbox4.color.setAlpha(1);
+
+// LADDERS
+ladder = new Sprite(50, 400, 15, 200, 'k')
+	ladder.color = 'rgb(255, 146, 146)';
+	ladder.strokeWeight = 0; 
 
 // PLAYER
-Circle = new Sprite(125, 25, 25, 25, 'd');
+Circle = new Sprite(350, 550, 25, 25, 'd');
 Circle.color = 'rgb(131, 79, 255)';
 
 // FOREGROUND
@@ -71,46 +82,64 @@ Circle.color = 'rgb(131, 79, 255)';
 	hitboxGroup.add(hitbox);
 	hitboxGroup.add(hitbox2);
 	hitboxGroup.add(hitbox3);
+    hitboxGroup.add(hitbox4);
+    ladderGroup.add(ladder);
 }
 	
-/*******************************************************/
-// draw()
-/*******************************************************/
+// DRAW
 function draw() {
 	background('rgb(235, 95, 95)'); 
-// Z space
 
-// text 
+
+// TEXT
 text("Mouse X = " + round(mouse.x), 350, 350);
 text("Mouse Y = " + round(mouse.y), 350, 375);
 
-// keyboard stuff
+// KEYBOARD
 Circle.vel.x = Circle.vel.x / 1.1;
 Circle.rotationSpeed = Circle.rotationSpeed / 1.05;
-	if (kb.pressing ('left')) {
+	if (kb.pressing ('a') && !kb.pressing ('e')) {
 
 		Circle.vel.x = -3;
 		Circle.rotationSpeed = -5;
 
 	} 
-	if (kb.pressing ('right')) {
+	if (kb.pressing ('d') && !kb.pressing ('e')){
 
 	Circle.vel.x = 3;
 	Circle.rotationSpeed = 5;
 	} 
-	if (kb.pressing ('up') && isOnFloor == 1) {
+    if (kb.pressing ('a') && kb.pressing ('e')) {
+
+		Circle.vel.x = -1;
+		Circle.rotationSpeed = -2.5;
+
+	} 
+	if (kb.pressing ('d') && kb.pressing ('e')){
+
+	Circle.vel.x = 1;
+	Circle.rotationSpeed = 2.5;
+	} 
+	if (kb.pressing ('space') && isOnFloor == 1) {
 
 	Circle.vel.y = -10;
 	} 
+
+    if (kb.pressing ('space') && isOnLadder == 1) {
+
+	Circle.vel.y = -5;
+	} 
+// COLLISION
 	 if (Circle.colliding(hitboxGroup)) {
  isOnFloor = 1
 } else if (!Circle.colliding(hitboxGroup)) {
  isOnFloor = 0
 }
 
+	 if (Circle.colliding(ladderGroup)) {
+ isOnLadder = 1
+} else if (!Circle.colliding(ladderGroup)) {
+ isOnLadder = 0
 }
 
-
-/*******************************************************/
-//  END OF APP
-/*******************************************************/
+}
